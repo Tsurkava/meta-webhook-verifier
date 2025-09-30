e0adfae74faf15a0d7f285b47a75d3d2d648a314
Tsurkava <122608207+Tsurkava@users.noreply.github.com>
2025-09-30 23:48:27 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const VERIFY_TOKEN = "tsurkava123"; // тот самый маркер
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token && mode === "subscribe" && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(3000, () => console.log("Webhook is listening on port 3000"));
